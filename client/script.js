fd82333b7773de56b68a35d5ece706f96ea394b9
Cesar Lantigua <cesar.lantigua@hotmail.com>
2023-01-10 16:30:24 -0500

    added render url

--- client/script.js
+++ client/script.js
@@ -99,7 +99,7 @@ const handleSubmit = async (e) => {
   loader(messageDIV);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://codebot-d8jd.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
